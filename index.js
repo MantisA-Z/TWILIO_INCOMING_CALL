d6b8bcc30506c1af1a6c6c16c9c599bfd56635c2
MantisA-Z <160739281+MantisA-Z@users.noreply.github.com>
2024-10-14 17:23:04 +0530

    added root route in the index.js

--- index.js
+++ index.js
@@ -3,6 +3,9 @@ const express = require("express");
 const receiveCallModel = require("./models/twilioIncomingCall");
 const app = express();
 
+app.get("/", (req, res) => {
+  res.end("hello");
+});
 app.post("/twilio/call", receiveCallModel);
 
 const PORT = process.env.PORT;
